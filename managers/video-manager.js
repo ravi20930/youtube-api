8e06c973950bdc0148e66be5ec4e746089975a8f
ravi <ravi.20930@gmail.com>
2022-07-13 09:15:05 +0530

    pagination in search results

--- managers/video-manager.js
+++ managers/video-manager.js
@@ -10,8 +10,24 @@ const getPagination = (page, size) => {
 };
 
 const getPagingData = (data, page, limit) => {
-    const assets = data;
-    const totalItems = data.length;
+    const { count: totalItems, rows: assets } = data;
+    const currentPage = page ? +page : 0;
+    const totalPages = Math.ceil(totalItems / limit);
+    return { limit, totalPages, currentPage, totalItems, assets };
+};
+const getPagingData2 = (data, page, limit) => {
+    // const { count: totalItems, rows: assets } = data;
+    let dataArray = [];
+    let array = data.forEach( (data) => {
+        dataArray.push({
+            "thumbs": data.thumbs,
+            "title": data.title,
+            "description": data.description,
+            "publishTime": data.publishTime
+        })
+    });
+    const assets = dataArray;
+    const totalItems = dataArray[0]?dataArray[0]['count(*) over()']:0;
     const currentPage = page ? +page : 0;
     const totalPages = Math.ceil(totalItems / limit);
     return { limit, totalPages, currentPage, totalItems, assets };
@@ -21,7 +37,7 @@ exports.videoData = async (req) => {
     try {
         const { page, size } = req.query;
         const { limit, offset } = getPagination(page, size);
-        const data = await videoModel.findAll({
+        const data = await videoModel.findAndCountAll({
             limit: limit,
             offset: offset,
             // where: {
@@ -52,8 +68,9 @@ exports.videoSearch = async (req) => {
         //     }
         // })]
         // 
-        const data = await sequelize.query(`select * from videos where MATCH (title, description) AGAINST('${q}' IN NATURAL LANGUAGE MODE) LIMIT ${limit} offset ${offset}`, { type: QueryTypes.SELECT });
-        const response = getPagingData(data, page, limit);
+        const data = await sequelize.query(`select *,count(*) over() from videos where MATCH (title, description) AGAINST('${q}' IN NATURAL LANGUAGE MODE) LIMIT ${limit} offset ${offset}`, { type: QueryTypes.SELECT });
+        // const response = getPagingData(data, page, limit);
+        const response = getPagingData2(data, page, limit);
         return response;
     } catch (err) {
         console.error(err.message);
